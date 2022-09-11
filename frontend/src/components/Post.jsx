464bf64966a176bc4dccfb08694d2ea5a62bac81
abenzoar yannis <abenzoaryannis.dev@gmail.com>
2022-09-11 05:07:29 +0200

    - Possibilite de supprimer les message pour le createur et les admins

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,15 +1,14 @@
+import PostFooter from "./PostFooter";
 
-
-const Post = ({ post }) => {
+const Post = ({ post, postId }) => {
   return (
     <div className="post">
       <p className="email">{post.userId}</p>
       <p className="date">{post.date}</p>
-      <p className="message">
-        {post.message}
-      </p>
+      <p className="message">{post.message}</p>
+      <PostFooter post={post} postId={postId} />
     </div>
-    );
+  );
 };
 
 export default Post;
--- frontend/src/components/PostFooter.jsx
+++ frontend/src/components/PostFooter.jsx
@@ -1,10 +1,45 @@
+import DataContext from "../context/DataContext";
+import { useContext } from "react";
+import axios from "axios";
 
+const PostFooter = ({ post, postId }) => {
+  const { posts, setPosts, auth, ROLES, postURL } = useContext(DataContext);
 
-const PostFooter = () => {
+  const deleteAPost = async (id) => {
+    try {
+      await axios.delete(`${postURL}/${id}`, {
+        headers: {
+          authorization: auth.accessToken,
+        },
+      });
+      const postsList = posts.filter((post) => post.id !== id);
+      setPosts(postsList);
+    } catch (err) {
+      if (err.response) {
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else {
+        console.log(`Error: ${err.message}`);
+      }
+    }
+  };
+
   return (
-    <div>
-        
-    </div>);
+    <div className="post-footer">
+      <div>
+        <button>Like</button>
+        <button>Dislike</button>
+      </div>
+      <div>
+        {auth.userId === post.userId ? <button>Modifier</button> : null}
+
+        {auth.role === ROLES.Admin || auth.userId === post.userId ? (
+          <button onClick={() => deleteAPost(postId)}>Supprimer</button>
+        ) : null}
+      </div>
+    </div>
+  );
 };
 
 export default PostFooter;
